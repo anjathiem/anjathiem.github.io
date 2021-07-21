3c0393fd9faa4bfb3a9e8402c448fb60e068c4d0
JabobKrauskopf <JabobKrauskopf@users.noreply.github.com>
2021-07-21 14:22:29 +0000

    deploy: bb747ad5264f2fe195b742405b3009bb38ec10d0

--- _next/static/AsvIhZBBE0ABp4pHh406R/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-376d8a1af6f65fd95640.js"],"/_error":["static/chunks/pages/_error-82a806cd39f8ab3dc3ac.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
